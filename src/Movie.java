88305eaf9bf674dbceaf29cab65c446eca1c46f1
Carla Jeanpierre <carlatresejeanpierre@gmail.com>
2023-08-14 23:20:32 -0400

    add Movie class with instance variables

---
+++ src/Movie.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private int id;
+    private String title;
+    private int releaseYear;
+    private String genre;
+    private List<String> actors;
+}
